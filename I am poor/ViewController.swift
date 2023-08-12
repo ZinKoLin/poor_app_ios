4ea04f5d6e432e5d5f03b90e6f1597ec555fff5f
ZinKoLin <zinkolin113@gmail.com>
2023-08-12 19:58:48 +0630

    first commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Zin Ko Lyn on 8/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
